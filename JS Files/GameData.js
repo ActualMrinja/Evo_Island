6872caa2da40d4d9414d7a8db565e1e6b97b02dd
ActualMrinja <42427718+ActualMrinja@users.noreply.github.com>
2019-08-15 21:08:55 -0500

    Update GameData.js

--- JS Files/GameData.js
+++ JS Files/GameData.js
@@ -347,11 +347,11 @@ mousedown = false;
   if (document.exitFullscreen) {
     document.exitFullscreen();
   } else if (document.mozCancelFullScreen) { /* Firefox */
-    document.mozCancelFullScreen();
-  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
-    document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) { /* IE/Edge */
-    document.msExitFullscreen();
+  /**  document.mozCancelFullScreen(); **/
+ /** } else if (document.webkitExitFullscreen) { **/ /* Chrome, Safari and Opera */
+ /**   document.webkitExitFullscreen(); ***/
+ /** } else if (document.msExitFullscreen) { /* IE/Edge */
+  /**  document.msExitFullscreen();**/
   }**/
   
      
